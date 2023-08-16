0aba3fae916b94d187aea56e1a618e29c097eae7
sandhya <arjasandhya@gmail.com>
2023-08-16 15:37:38 +0530

    sample programs

---
+++ word.py
@@ -0,0 +1,16 @@
+ s="sandhya"
+for i in range(1,len(s)+1):
+    print(s[:i])
+            
+            
+
+a=int(input())
+b=int(input())
+x=lambda a,b:a*b 
+print(x(a,b))
+m=lambda a:a*a*a 
+print(m(a))
+
+
+
+
